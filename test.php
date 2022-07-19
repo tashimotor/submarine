a4f9cdca1b8e78443e02f3765630e36417c27271
Alexander Proshlyakov <tashimotor@gmail.com>
2022-07-19 11:53:56 +0300

    Рефакторинг

--- test.php
+++ test.php
@@ -2,35 +2,59 @@
 //Функция создаёт заказ в БД интернет магазина.
 //Задача: указать все ошибки и недостатки, сделать рефакторинг.
 
-function MK_ord()
+function MK_ord(): void
 {
-    $user_id = $_GET['user'];
+    $user_id = (int) $_GET['user'];
     $items   = $_GET['item'];
     $prices  = $_GET['price'];
 
     $link = mysqli_connect('localhost', 'mysql_user', 'mysql_password');
-    if (!$link) {
+    if (!$link || !mysqli_set_charset($link, "utf8mb4")) {
         die('Ошибка');
     }
 
+    $orderSum   = 0;
+    $orderItems = [];
+
     foreach ($items as $key => $value) {
-        $query   = mysqli_query($link, "SELECT balance FROM users WHERE id = '$user_id'");
+        $itemPrice = $prices[$key] ?? die('Ошибка');
+        $itemPrice = (float) $itemPrice;
+
+        $query = mysqli_query(
+            $link,
+            sprintf('SELECT balance FROM users WHERE id = %d', $user_id)
+        );
+
         $balance = mysqli_fetch_row($query);
 
-        if ($prices[$key] < $balance) {
-            $sql = "UPDATE users SET balance = balance - ".$prices[$key];
-            if (!mysqli_query($link, $sql)) {
-                die('Ошибка');
-            }
-            $order_items[] = $value;
-            $order_price   += $prices[$key];
+        if ($itemPrice > $balance) {
+            break;
         }
+
+        if (!mysqli_query(
+            $link,
+            sprintf('UPDATE users SET balance = balance - %d WHERE user_id=%d', $itemPrice, $user_id)
+        )) {
+            die('Ошибка');
+        }
+
+        $orderItems[] = $value;
+        $orderSum     += $itemPrice;
     }
 
-    $sql = "INSERT INTO orders ($user_id, $items, $sum) 
-			VALUES ($user_id, '".implode(';', $order_items)."', $order_price)";
+    if (count($orderItems) === 0) {
+        die('Ошибка');
+    }
 
-    if (!mysqli_query($link, $sql)) {
+    if (!mysqli_query(
+        $link,
+        sprintf(
+            "INSERT INTO orders ('user_id', 'items', 'sum') VALUES (%d, '%s', %d)",
+            $user_id,
+            mysqli_real_escape_string($link, implode(';', $orderItems)),
+            $orderSum
+        )
+    )) {
         die('Ошибка');
     }
 
